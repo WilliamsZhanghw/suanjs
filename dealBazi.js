1c3fc1197c8776f2d726d077a0cd708e33a8009d
WilliamsZhanghw <Williamszhanghw@gmail.com>
2025-01-05 17:24:22 -0600

    Update dealBazi.js

--- dealBazi.js
+++ dealBazi.js
@@ -54,4 +54,30 @@ function transformGanZhiToWuXing(inputString) {
 
     return result;
 }
+function determineConfidence(inputString) {
+   
+    const conditions = {
+        '金': ['金', '土'], // 金的前一个是土
+        '水': ['水', '金'], // 水的前一个是金
+        '木': ['木', '水'], // 木的前一个是水
+        '火': ['火', '木'], // 火的前一个是木
+        '土': ['土', '火']  // 土的前一个是火
+    };
+
+    // 获取第4、5、6个字
+    const element4 = inputString[3]; // 第4个字
+    const element5 = inputString[4]; // 第5个字
+    const element6 = inputString[5]; // 第6个字
+
+    // 判断逻辑
+    if (
+        element5  &&
+        (conditions[element5].includes(element4) || conditions[element5].includes(element6))
+    ) {
+        return 'confident'; // 返回英文的“自信”
+    } else {
+        return 'not confident'; // 返回英文的“不自信”
+    }
+}
+
 
